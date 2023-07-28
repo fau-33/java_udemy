a7a4f64d548b81f9dfc886d4a347a5da531405e7
Flávio Félix <fau_felix30@hotmail.com>
2023-07-27 21:06:25 -0300

    Feito as aulas sobre primitivos e notação ponto

---
+++ src/fundamentos/Inferencia.java
@@ -0,0 +1,23 @@
+package fundamentos;
+
+public class Inferencia {
+	public static void main(String[] args) {
+		double a = 4.5;
+		System.out.println(a);
+		
+		var b = 4.5;
+		System.out.println(b);
+		
+		var c = "Texto";
+		System.out.println(c);
+		
+		c = "Outro texto";
+		System.out.println(c);
+		
+		// c = 4.5;
+		
+		double d; // Variável foi declarada
+		d = 123.5; // variável inicializada
+		System.out.println(d);
+	}
+}
---
+++ src/fundamentos/tiposPrimitivos.java
@@ -0,0 +1,39 @@
+package fundamentos;
+
+public class tiposPrimitivos {
+
+	public static void main(String[] args) {
+		// Informações de funcionários
+		
+		// Tipos númericos inteiros
+		byte anosDeEmpresa = 23;
+		short numeroDeVoos = 545;
+		int id = 56789;
+		long pontosAcumulados = 3_234_845_223L;
+		
+		// Tipos númericos reais
+		float salario = 11_334.44F;
+		double vendasAcumuladas = 2_991_797_103.01;
+		
+		// Tipo booleano
+		boolean estaDeFerias = false;// true
+		
+		// Tipo caractere
+		char status = 'A';// Ativo
+		
+		// Dias da semana
+		System.out.println(anosDeEmpresa * 365);
+		
+		// Números de vôos
+		System.out.println(numeroDeVoos / 2);
+		
+		// Pontos por real
+		System.out.println(pontosAcumulados / vendasAcumuladas);
+		
+		System.out.println(id + " ganha -> " + salario);
+		System.out.println("Férias? " + estaDeFerias);
+		System.out.println("Status: " + status);
+
+	}
+
+}
