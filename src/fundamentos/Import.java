10dc20956832e82656a2a3f5cafd1fc76f5c6584
Flávio Félix <fau_felix30@hotmail.com>
2023-08-02 19:12:56 -0300

    Feito as aulas fundamentos sobre console, import e tipos de string

---
+++ src/fundamentos/Import.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+import java.util.Date;
+
+// import javax.swing.JButton;
+
+public class Import {
+	public static void main(String[] args) {
+		java.lang.String b = "Bom dia!";
+		java.lang.System.out.println(b);
+		
+		String s = "Boa tarde!";
+		System.out.println(s);
+		
+		Date d = new Date();
+		System.out.println(d);
+		
+		// JButton botao = new JButton();
+	}
+}
