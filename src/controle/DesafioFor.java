091810b3e4107226a0961875e88d80a94c57be2b
Flávio Félix <fau_felix30@hotmail.com>
2024-06-11 19:35:25 -0300

    Feito o desafio for e aulas sobre for e switch

---
+++ src/controle/DesafioFor.java
@@ -0,0 +1,16 @@
+package controle;
+
+public class DesafioFor {
+	public static void main(String[] args) {
+		;
+		
+		for(String valor ="#";!valor.equals("######");valor += "#") {
+			System.out.println(valor);
+			
+		}
+		
+		// Versão desafio
+		// Não pode usar valor numérico para controlar o laço
+	}
+
+}
---
+++ src/controle/For2.java
@@ -0,0 +1,14 @@
+package controle;
+
+public class For2 {
+	public static void main(String[] args) {
+		int numeroInicial = 10;
+		
+		for(int i = numeroInicial; i >= 0; i-=2) {
+			System.out.println("O contador = " + i);
+		}
+		
+		System.out.println("Fim!");
+	}
+
+}
