43740cbe22b8ff4ea34f568a159e61ff291dee22
Flávio Félix <fau_felix30@hotmail.com>
2024-06-10 22:01:27 -0300

    Feito desafio while java

---
+++ src/controle/DoWhile.java
@@ -0,0 +1,23 @@
+package controle;
+
+import java.util.Scanner;
+
+public class DoWhile {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+			String texto = "";
+			
+		 do {
+			 System.out.println("Você precisa falar "
+		 + "\nas palavras mágicas...");
+			 System.out.println("Quer sair? ");
+			 texto = entrada.nextLine();
+			 
+		 }while(!texto.equalsIgnoreCase("por favor"));
+		
+		entrada.close();
+		
+		System.out.println("Obrigado!");
+	}
+
+}
