e4d5000c4f617fdc269cc8c9b6a2141e7585dc6f
Flávio Félix <fau_felix30@hotmail.com>
2024-07-16 20:00:28 -0300

    Feito as aulas sobre membros de instância e classe

--- src/classe/ProdutoTeste.java
+++ src/classe/ProdutoTeste.java
@@ -3,16 +3,18 @@ package classe;
 public class ProdutoTeste {
 	public static void main(String[] args) {
 		
-		Produto p1 = new Produto("Notebook", 4354.98, 0.25);
+		Produto p1 = new Produto("Notebook", 4354.98);
 		
 		
 		var p2 = new Produto();
 		p2.nome = "Caneta Preta";
 		p2.preco = 12.56;
-		p2.desconto = 0.29;
 		
-		System.out.println(p1.nome);
-		System.out.println(p2.nome);
+		
+		Produto.desconto = 0.50;
+		
+		System.out.println(p1.nome + " " + p1.precoComDesconto());
+		System.out.println(p2.nome + " " + p2.precoComDesconto());
 		
 		double descontoFinal1 = p1.precoComDesconto();
 		double descontoFinal2 = p2.precoComDesconto(0.1);
